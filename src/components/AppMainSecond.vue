95176d40d2e172a6cb39865ca0cb80984223ca97
francescofalcone05 <ciccio_97@live.it>
2024-05-23 18:33:39 +0300

    second Main compleated

--- src/components/AppMainSecond.vue
+++ src/components/AppMainSecond.vue
@@ -1,26 +1,52 @@
 <script>
 
+export default {
+    data() {
+        return {
+            otherPages: [
+                {
+                    text: 'DIGITAL COMICS',
+                    immagine: '../../img/buy-comics-digital-comics.png'
+                },
+                {
+                    text: 'DC MERCHANDISE',
+                    immagine: '../../img/buy-comics-merchandise.png'
+                },
+                {
+                    text: 'SUBSCRIPTION',
+                    immagine: '../../img/buy-comics-subscriptions.png'
+                },
+                {
+                    text: 'COMIC SHOP LOCATOR',
+                    immagine: '../../img/buy-comics-shop-locator.png'
+                },
+                {
+                    text: 'DC POWER VISA',
+                    immagine: '../../img/buy-dc-power-visa.svg'
+                }
+            ]
+        }
+    }
+}
+
 
 </script>
 
 <template>
     <div class="contenitore">
         <div class="d-flex" style="height: 100%;">
-            <div class="single-card align-middle">
-                card singola
-            </div>
-            <div class="single-card">
-                card singola
-            </div>
-            <div class="single-card">
-                card singola
-            </div>
-            <div class="single-card">
-                card singola
-            </div>
-            <div class="single-card">
-                card singola
-            </div>
+            <template v-for="page in otherPages">
+                <div class="single-card d-flex justify-content-center py-4">
+                    <div class="align-content-center">
+                        <img :src="page.immagine" alt="">
+                    </div>
+                    <div class="align-content-center px-2 my_fs">
+                        {{ page.text }}
+                    </div>
+                </div>
+            </template>
+
+
         </div>
 
     </div>
@@ -34,9 +60,15 @@
 
 .single-card {
     width: calc(100% / 5);
-    border: solid 1px black;
     text-align: center;
     height: 100%;
-    align-content: center;
+}
+
+img {
+    height: 100%;
+}
+
+.my_fs {
+    font-size: 0.7rem;
 }
 </style>
